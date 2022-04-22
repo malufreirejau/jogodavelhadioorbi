78404d49c2a26f2893574f7e8157db95dc0c154d
unknown <malufreirejau@yahoo.com.br>
2022-04-22 14:15:47 -0300

    adding player 2

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ mudarJogador("X");
 function escolherQuadrado(id){
     var quadrado = document.getElementById(id);
 
+    if (quadrado.innerHTML !== "-") {
+        return;
+    }
+
     quadrado.innerHTML = jogador;
     quadrado.style.color = '#F908F2';
+
+    if (jogador === "X") {
+        jogador = "O";
+    } else {
+        jogador = "X";
+    }
+    mudarJogador(jogador);
 }
 
 function mudarJogador(valor) {
